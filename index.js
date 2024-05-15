1a148ea29c10685046959d6af0f2f120d28ccd5f
Harshavardhan K <harshavardhan75anc@gmail.com>
2024-05-15 10:41:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const app = express();
+const PORT =9000;
+app.get('/',(req,res)=>{
+    res.send('Welcome to Oxynes');
+});
+app.get('/about',(req, res)=>{
+    res.send('<h2>About Page 101</h2>');
+});
+app.get('/contact',(req, res)=>{
+    res.json({
+        name:'Oxynes',
+        email:'XXXXXXXXXXXXXXXX',
+        number: "91 9392301126"
+    });
+});
+app.get('/help',(req, res)=>{
+    res.send('Help Page');
+});
+app.get('/home',(req, res)=>{
+    res.send('Home Page');
+});
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
