f5a329f3bca2e90c7c816cbaf94412b306df25f2
Harshavardhan K <harshavardhan75anc@gmail.com>
2024-05-15 11:01:16 +0530

    -

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express =require('express');
 const app = express();
 const PORT =9000;
+const mongoose = require('mongoose');
+
+let MONGODB_URL="mongodb://localhost:27017/url_shortener"
+mongoose.connect(MONGODB_URL)
+.then(()=>{
+    console.log('Connected to Database');
+})
+.catch((err)=>{
+    console.log(err.message);
+})
+
 app.get('/',(req,res)=>{
     res.send('Welcome to Oxynes');
 });
